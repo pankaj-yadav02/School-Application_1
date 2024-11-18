a39289923cb54ad0650bc82a78d571e5a99c0f71
“pankaj-yadav02” <py21197@outlook.com>
2024-11-18 14:32:24 +0530

    This is the 2nd School Application

---
+++ src/main/java/com/thrivesup/test/SchoolApplication1Application.java
@@ -0,0 +1,13 @@
+package com.thrivesup.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolApplication1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolApplication1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/thrivesup/test/SchoolApplication1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thrivesup.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolApplication1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
