5b6d581f753e98e502dfa6a0769cae0cdd09faad
pankaj-yadav02 <120123596+pankaj-yadav02@users.noreply.github.com>
2024-11-19 13:22:24 +0530

    Added a new name field

--- src/main/java/com/thrivesup/test/SchoolApplication1Application.java
+++ src/main/java/com/thrivesup/test/SchoolApplication1Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SchoolApplication1Application {
-
+	private String name;
 	public static void main(String[] args) {
 		SpringApplication.run(SchoolApplication1Application.class, args);
 	}
